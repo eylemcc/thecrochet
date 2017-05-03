eea0219dcd0385cb73dbea9aa250bf9c391a4248
Eylem Can Cetintas <eylemcan@gmail.com>
2017-05-03 14:13:46 +0200

    no message

--- auto_slider.js
+++ auto_slider.js
@@ -2,34 +2,36 @@
  * Created by eylemcancetintas on 18.03.17.
  */
 
+$(document).ready(function () {
+    var slider = function () {
+        var slides = document.querySelectorAll('#slides .slide');
+        var currentSlide = 0;
+        var slideInterval = setInterval(nextSlide, 7000);
 
-var slider = function () {
-    var slides = document.querySelectorAll('#slides .slide');
-    var currentSlide = 0;
-    var slideInterval = setInterval(nextSlide, 7000);
+        function nextSlide() {
+            goToSlide(currentSlide + 1);
+        }
 
-    function nextSlide() {
-        goToSlide(currentSlide+1);
-    }
+        function previousSlide() {
+            goToSlide(currentSlide - 1);
+        }
 
-    function previousSlide() {
-        goToSlide(currentSlide-1);
-    }
+        function goToSlide(n) {
+            slides[currentSlide].className = 'slide';
+            currentSlide = (n + slides.length) % slides.length;
+            slides[currentSlide].className = 'slide showing';
+        }
 
-    function goToSlide(n) {
-        slides[currentSlide].className = 'slide';
-        currentSlide = (n+slides.length)%slides.length;
-        slides[currentSlide].className = 'slide showing';
-    }
-    var next = document.getElementById('next');
-    var previous = document.getElementById('previous');
+        var next = document.getElementById('next');
+        var previous = document.getElementById('previous');
 
-    next.onclick = function() {
-        //pauseSlideshow();
-        nextSlide();
+        next.onclick = function () {
+            //pauseSlideshow();
+            nextSlide();
+        };
+        previous.onclick = function () {
+            //pauseSlideshow();
+            previousSlide();
+        };
     };
-    previous.onclick = function() {
-        //pauseSlideshow();
-        previousSlide();
-    };
-};
+}
